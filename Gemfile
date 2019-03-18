5b9e591ed9fe26a30026cc1660352ae2923fc76e
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2019-03-18 06:28:58 -0300

    WIP - implementing rx

--- Gemfile
+++ Gemfile
@@ -3,4 +3,6 @@ source 'https://rubygems.org'
 # gem 'eventmachine'
 # gem install eventmachine --platform ruby
 gem 'em-websocket'
+gem 'rx'
+gem 'rufus-scheduler'
 gem 'websocket-client-simple'
--- server.rb
+++ server.rb
@@ -1,8 +1,25 @@
 require 'em/pure_ruby'
 require 'em-websocket'
+require 'rx'
 
 EM.run do
+  messages = []
+
   EM::WebSocket.run(host: '0.0.0.0', port: 8080) do |ws|
+
+    source = Rx::Observable.from_array(messages)
+    subscription = source.subscribe(
+        lambda {|x|
+          puts 'Next: ' + x.to_s
+        },
+        lambda {|err|
+          puts 'Error: ' + err.to_s
+        },
+        lambda {
+          puts 'Completed'
+        })
+
+
     ws.onopen do |handshake|
       puts 'WebSocket connection open'
 
@@ -13,10 +30,14 @@ EM.run do
       ws.send "Hello Client, you connected to #{handshake.path}"
     end
 
-    ws.onclose { puts 'Connection closed' }
+    ws.onclose do
+      puts 'Connection closed'
+      subscription.unsubscribe
+    end
 
     ws.onmessage do |msg|
       puts "Received message: #{msg}"
+      messages << msg
       ws.send "Pong: #{msg}"
     end
   end
